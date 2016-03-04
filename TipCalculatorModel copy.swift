9c5845019bb619c5fb02f874c1db587e8a196712
hpine19 <hpine19@priorypride.net>
2016-03-04 09:36:54 -1000

    Tip Calculator
    This program was made using Xcode (swift). The user inputs the
    percentage of tip he/she would like to calculate.

---
+++ TipCalculatorModel copy.swift
@@ -0,0 +1,45 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Hypatia Pine on 11/19/15.
+//  Copyright © 2015 Hypatia Pine. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+    
+}
